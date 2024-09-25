118b1a391fc79097412f9a9a2b928fbecd5981e4
YEOM <yyeom1995@gmail.com>
2024-09-25 14:12:03 +0900

    0925 LogoutServlet 추가

---
+++ src/main/java/servlet/LogoutServlet.java
@@ -0,0 +1,29 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession session = req.getSession();
+		session.invalidate(); // 세션을 강제 종료
+		System.out.println("로그아웃 성공");
+		// 페이지 이동
+		resp.sendRedirect("/TeamProject/views/login.jsp"); // 로그인 페이지로 이동
+		
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}	
+}
--- src/main/webapp/views/dashboard.jsp
+++ src/main/webapp/views/dashboard.jsp
@@ -201,7 +201,7 @@
                         <div class="col-12 col-md-6 order-md-2 order-first">
                             <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
                                 <ol class="breadcrumb">
-                                    <li class="breadcrumb-item"><a href="login.jsp">로그아웃</a></li>
+                                    <li class="breadcrumb-item"><a href="/TeamProject/logout">로그아웃</a></li> 
                                 </ol>
                             </nav>
                         </div>
